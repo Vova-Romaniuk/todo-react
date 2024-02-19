3052feaa33443c938cc4e5b484a03fd5762f7789
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-02-19 22:37:14 +0200

    complete dropdown and fetch data about race

--- app/src/components/TodoPage/index.tsx
+++ app/src/components/TodoPage/index.tsx
@@ -7,6 +7,8 @@ import { generateUniqueId } from "../../helpers/date";
 import Dialog from "../Dialog";
 import DatePicker from "../DatePicker";
 import { formatDateTime } from "../../helpers/date";
+import { TaskType } from "../../models/task";
+import Dropdown from "../Dropdown";
 
 function TodoPage() {
 	const [tasks, setTasks] = useState<TaskModel[]>([]);
@@ -14,6 +16,8 @@ function TodoPage() {
 	const [isShowDialog, setIsShowDialog] = useState<boolean>(false);
 	const [isOpenDatePicker, setIsOpenDatePicker] = useState<boolean>(false);
 	const [deadline, setDeadline] = useState<string>("");
+	const [filterTasksByType, setFilterTasksByType] = useState<TaskModel[]>([]);
+	const [typeTask, setTypeTask] = useState<string>("task");
 
 	useEffect(() => {
 		const storedTasks = localStorage.getItem("tasks");
@@ -32,6 +36,7 @@ function TodoPage() {
 					id: generateUniqueId(),
 					description: trimmedDescription,
 					isDone: false,
+					type: TaskType.Task,
 					deadline: deadline ? deadline : "no deadline",
 				},
 			]);
@@ -70,6 +75,14 @@ function TodoPage() {
 		}
 	}, [tasks]);
 
+	useEffect(() => {
+		setFilterTasksByType(tasks.filter((item) => item.type === typeTask));
+	}, [typeTask]);
+
+	const handleChangeTypeTask = (value) => {
+		setTypeTask(value);
+	};
+
 	return (
 		<div className='w-full h-screen bg-[#0d0d0d] flex flex-col'>
 			{isShowDialog && (
@@ -83,32 +96,43 @@ function TodoPage() {
 								onChange={handleChange}
 								className='!h-14 focus:border-[1px] focus:border-textColor'
 							/>
-							<div className='text-xl text-textColor mt-6 flex'>
-								<div className='flex items-center text-lg'>
-									<span>Select deadline for your task:</span>
-									<span className='underline ml-2 text-textInput text-lg'>
-										{formatDateTime(deadline) ===
-										"Invalid Date"
-											? "nothing selected"
-											: formatDateTime(deadline)}
-									</span>
+							{typeTask === "task" ? (
+								<div className='text-xl text-textColor mt-6 flex'>
+									<div className='flex items-center text-lg'>
+										<span>
+											Select deadline for your task:
+										</span>
+										<span className='underline ml-2 text-textInput text-lg'>
+											{formatDateTime(deadline) ===
+											"Invalid Date"
+												? "nothing selected"
+												: formatDateTime(deadline)}
+										</span>
+									</div>
+									<div className='w-fit h-fit relative ml-auto '>
+										{isOpenDatePicker && (
+											<DatePicker
+												handleClose={
+													handleToggleOpenDatePicker
+												}
+												setDeadline={setDeadline}
+											/>
+										)}
+										<Button
+											onClick={handleToggleOpenDatePicker}
+											className='text-3xl !text-textColor border-0 outline-0'>
+											<i className='fa-regular fa-calendar-days'></i>
+										</Button>
+									</div>
 								</div>
-								<div className='w-fit h-fit relative ml-auto '>
-									{isOpenDatePicker && (
-										<DatePicker
-											handleClose={
-												handleToggleOpenDatePicker
-											}
-											setDeadline={setDeadline}
-										/>
-									)}
-									<Button
-										onClick={handleToggleOpenDatePicker}
-										className='text-3xl !text-textColor border-0 outline-0'>
-										<i className='fa-regular fa-calendar-days'></i>
-									</Button>
-								</div>
-							</div>
+							) : (
+								typeTask === "reminder" && (
+									<div className=''>
+										<Dropdown name='Select your favorite race' />
+									</div>
+								)
+							)}
+
 							<div className='mt-auto flex justify-center gap-10 !text-2xl'>
 								<Button
 									className='w-fit !h-12 !px-4 !py-2 !border-accentColor duration-200 border-2 hover:bg-accentColor hover:text-black rounded-2xl !text-xl'
@@ -145,7 +169,27 @@ function TodoPage() {
 						</div>
 					</div>
 				</div>
-				<div className='h-1/6 w-full flex justify-center'>
+				<div className='h-1/6 w-full flex justify-between items-center'>
+					<div className='my-auto w-fit flex !text-2xl'>
+						<Button
+							onClick={() => handleChangeTypeTask(TaskType.Task)}
+							className={`hover:border-b-2 w-fit border-transparent border-b-2 hover:border-accentColor pb-2 !h-10 ${
+								typeTask === TaskType.Task &&
+								"!border-accentColor"
+							}`}>
+							Tasks
+						</Button>
+						<Button
+							onClick={() =>
+								handleChangeTypeTask(TaskType.Reminder)
+							}
+							className={`hover:border-b-2 ml-10 w-fit border-transparent border-b-2 hover:border-accentColor pb-2 !h-10 ${
+								typeTask === TaskType.Reminder &&
+								"!border-accentColor"
+							}`}>
+							Reminders
+						</Button>
+					</div>
 					<div className='h-12 w-full m-auto flex'>
 						<Button
 							onClick={handleOpenDialog}
@@ -157,7 +201,7 @@ function TodoPage() {
 
 				<div className='h-3/6 w-full flex flex-col overflow-y-auto'>
 					<div className='h-fit flex flex-col gap-10 pt-10'>
-						{tasks.map((item) => (
+						{filterTasksByType?.map((item) => (
 							<Task
 								description={item.description}
 								key={item.id}
--- app/src/models/task.ts
+++ app/src/models/task.ts
@@ -1,6 +1,12 @@
+export enum TaskType {
+	Task = "task",
+	Reminder = "reminder",
+}
+
 export interface TaskModel {
 	id: string;
 	description: string;
 	isDone?: boolean;
+	type: TaskType;
 	deadline: string;
 }
