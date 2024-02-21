4adaeeb6937aa114c650884a0c7c15fa0a16caca
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-02-21 11:41:09 +0200

    delete useEffect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,6 @@
 import { useEffect } from "react";
 import TodoPage from "./components/TodoPage";
 function App() {
-	useEffect(() => {
-		const storedData = localStorage.getItem("todoData");
-		if (!storedData) {
-			localStorage.setItem("todoData", JSON.stringify([]));
-		}
-	}, []);
-
 	return (
 		<div className='w-screen h-fit flex'>
 			<TodoPage />
